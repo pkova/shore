79b3ec08004090cb29d129ff95211b9c16d31cf6
pkova <pyry.kovanen@gmail.com>
2022-07-19 16:14:17 +0300

    Implement graveyard, launch-instance, and cors-approve

--- api/src/shore/main.clj
+++ api/src/shore/main.clj
@@ -7,6 +7,7 @@
             [datomic.ion.dev :as ion]
             [datomic.ion.cast :as cast]
             [datomic.ion.dev :as dev]
+            [clojure.java.io :as io]
             [cognitect.aws.client.api :as aws]))
 
 (def cfg {:server-type :ion
@@ -14,10 +15,16 @@
           :system "shore"
           :endpoint "https://ja6vetvux9.execute-api.us-east-2.amazonaws.com"})
 
+(def userdata-template (slurp (io/resource "userdata.sh")))
+
 (def get-client (memoize (fn [] (d/client cfg))))
 
 (def ec2 (aws/client {:api :ec2}))
+(def ssm (aws/client {:api :ssm}))
 (def route53 (aws/client {:api :route53}))
+
+(defn b64-encode [s]
+  (.encodeToString (java.util.Base64/getEncoder) (.getBytes s)))
 
 (defn safe-read-str [s]
   (try (json/read-str s)
@@ -29,7 +36,8 @@
         db     (d/db conn)
         ticket (ffirst (d/q '[:find ?t
                               :in $ ?t
-                              :where [_ :shore/ticket ?t]]
+                              :where [?e :ticket/patq ?t]
+                                     [?e :ticket/assigned false]]
                             db
                             (get (safe-read-str (slurp body)) "ticket" "")))]
     (merge {:headers {"Access-Control-Allow-Origin" "https://shore.arvo.network"
@@ -40,10 +48,18 @@
        (not= (= uri "/enter"))     {:status 404}
        (nil? ticket)               {:status 403}
 
-       :else {:status 200
-              :body
-              (json/write-str {:url "https://halbex-palheb.arvo.network"
-                               :code "rontud-bannus-wismeg-roswer"})}))))
+       :else
+       (let [[urbit-id code] (first (d/q '[:find ?u ?c
+                                           :where [?e :ship/redeemed false]
+                                                  [?e :ship/instance ?i]
+                                                  [?i :instance/certificate-acquired true]
+                                                  [?e :ship/urbit-id ?u]
+                                                  [?e :ship/code ?c]
+                                           ] db))]
+           {:status 200
+            :body
+            (json/write-str {:url (format "https://%s.arvo.network" urbit-id)
+                             :code code})})))))
 
 (defn rand-patq []
   (-> (repeatedly 8 (fn [] (unchecked-byte (rand-int 256))))
@@ -52,7 +68,7 @@
       ob/biginteger->patq))
 
 (defn add-tickets [n conn]
-  (->> (repeatedly n (fn [] {:shore/ticket (rand-patq)}))
+  (->> (repeatedly n (fn [] {:ticket/patq (rand-patq) :ticket/assigned false}))
        (assoc {} :tx-data)
        (d/transact conn)))
 
@@ -70,5 +86,104 @@
          :Type "A"
          :TTL 300
          :ResourceRecords [{:Value ip}]}}]}}}))
+
+(defn get-auth-cookie [ip code]
+  (-> (http/post (str "http://" ip "/~/login") {:form-params {:password code}})
+      :headers
+      (get "set-cookie")
+      (str/split #";")
+      first))
+
+(defn launch-instance [urbit-id networking-key]
+  (aws/invoke
+   ec2
+   {:op :RunInstances
+    :request
+    {:InstanceType "t2.micro"
+     :MinCount 1
+     :MaxCount 1
+     :KeyName "pyry"
+     :ImageId "ami-02d1e544b84bf7502"
+     :IamInstanceProfile {:Arn "arn:aws:iam::852127795312:instance-profile/shore-role"}
+     :BlockDeviceMappings [{:DeviceName "/dev/xvda" :Ebs {:VolumeSize 15
+                                                         :VolumeType "gp2"
+                                                         :SnapshotId "snap-0896fac2267d1c5f1"
+                                                         :DeleteOnTermination true}}]
+     :SecurityGroupIds ["sg-02dcf91b9b7958722"]
+     :UserData (b64-encode (format userdata-template
+                                   (subs urbit-id 1)
+                                   networking-key))
+     :TagSpecifications [{:ResourceType "instance"
+                          :Tags [{:Key "Name" :Value urbit-id}
+                                 {:Key "Group" :Value "shore"}]}]}}))
+
+(defn get-public-ip [instance-id]
+  (get-in (aws/invoke ec2 {:op :DescribeInstances
+                           :request {:InstanceIds [instance-id]}})
+          [:Reservations 0 :Instances 0 :NetworkInterfaces 0 :Association :PublicIp]))
+
+(defn cors-approve [ip cookie urbit-id]
+  (http/put (str "http://" ip "/~/channel/shore-cors-approve")
+            {:headers {"cookie" cookie
+                       "content-type" "application/json"}
+             :body
+             (json/write-str
+              [{:id 0
+                :action "poke"
+                :ship (subs urbit-id 1)
+                :app "herm"
+                :mark "belt"
+                :json {:txt (str/split "|cors-approve 'https://shore.arvo.network'" #"")}}
+               {:id 1
+                :action "poke"
+                :ship (subs urbit-id 1)
+                :app "herm"
+                :mark "belt"
+                :json {:ret nil}}
+               {:id 2
+                :action "poke"
+                :ship (subs urbit-id 1)
+                :app "herm"
+                :mark "belt"
+                :json {:txt (str/split (str ":acme &path /network/arvo/" (subs urbit-id 1)) #"")}}
+               {:id 3
+                :action "poke"
+                :ship (subs urbit-id 1)
+                :app "herm"
+                :mark "belt"
+                :json {:ret nil}}])}))
+
+(defn graveyard-instance [instance-id urbit-id]
+  (let [no-sig (subs urbit-id 1)]
+    (aws/invoke
+     ssm
+     {:op :SendCommand
+      :request {:DocumentName "AWS-RunShellScript"
+                :InstanceIds [instance-id]
+                :Parameters
+                {"commands" ["pkill screen"
+                             "sleep 5"
+                             (format "tar -Scvzf /root/urbit/%s.tar.gz /root/urbit/%s" no-sig no-sig)
+                             (format "aws s3 cp /root/urbit/%s.tar.gz s3://shore-graveyard" no-sig)
+                             (format "aws ec2 terminate-instances --region us-east-2 --instance-ids %s" instance-id)]}}})))
+
+(defn birth-instance [db conn]
+  (let [{:keys [db/id
+                ship/urbit-id
+                ship/code
+                ship/networking-key]} (ffirst (d/q '[:find (pull ?e [*])
+                                                         :where [?e :ship/redeemed false]] db))]
+    (println urbit-id)
+    #_(d/transact conn {:tx-data [[:db/add id :ship/redeemed true]]})
+    (let [instance-id (get-in (launch-instance urbit-id networking-key)
+                              [:Instances 0 :InstanceId])]
+      (println instance-id)
+      (Thread/sleep 10000)
+      (create-record (get-public-ip instance-id) (str (subs urbit-id 1) ".arvo.network"))
+      (Thread/sleep 900000)
+      (let [ip (get-public-ip instance-id)]
+        (cors-approve ip (get-auth-cookie ip code) urbit-id)
+        (d/transact conn {:tx-data [{:instance/id instance-id :instance/assigned false}]})
+        (d/transact conn {:tx-data [[:db/add [:ship/urbit-id urbit-id] :ship/instance [:instance/id instance-id]]]})))))
 
 ;; (def conn (d/connect (get-client) {:db-name "shore"}))
--- api/src/shore/schema.clj
+++ api/src/shore/schema.clj
@@ -1,11 +1,15 @@
 (ns shore.schema)
 
-(def shore-schema
-  [{:db/ident :shore/ticket
+(def ticket-schema
+  [{:db/ident :ticket/patq
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
     :db/unique :db.unique/identity
-    :db/doc "@q formatted ticket for Shore, completely separate from L2 Bridge Activation ticket."}])
+    :db/doc "@q formatted ticket for Shore, completely separate from L2 Bridge Activation ticket."}
+   {:db/ident :ticket/redeemed-at
+    :db/valueType :db.type/instant
+    :db/cardinality :db.cardinality/one
+    :db/doc "The timestamp of when the Shore ticket was redeemed."}])
 
 (def user-schema
   [{:db/ident :user/email
@@ -13,19 +17,54 @@
     :db/cardinality :db.cardinality/one
     :db/unique :db.unique/identity
     :db/doc "Email of the user."}
+   {:db/ident :user/ticket
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Reference to the ticket that the user redeemed"}
    {:db/ident :user/ship
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Reference to the ship the user was assigned"}])
+
+(def ship-schema
+  [{:db/ident :ship/urbit-id
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity
     :db/doc "@p for the ship, always a L2 planet."}
-   {:db/ident :user/activation-ticket
+   {:db/ident :ship/activation-ticket
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/doc "L2 Activation ticket."}
-   {:db/ident :user/code
+    :db/doc "L2 Activation master ticket."}
+   {:db/ident :ship/networking-key
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/doc "L2 Activation ticket."}
-   {:db/ident :user/cookie
+    :db/doc "The Urbit networking key for booting."}
+   {:db/ident :ship/code
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/doc "Authenticated login cookie for the ship."}])
+    :db/doc "Urbit access +code."}
+   {:db/ident :ship/redeemed
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "Whether the ship has been redeemed by a user."}
+   {:db/ident :ship/instance
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Reference to the instance running the ship, if any."}])
+
+(def instance-schema
+  [{:db/ident :instance/id
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity
+    :db/doc "The AWS provided instance ID."}
+   {:db/ident :instance/assigned
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "Whether the instance has been assigned to running a ship."}
+   {:db/ident :instance/certificate-acquired
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc "Whether /app/acme has acquired the TLS certificate for HTTPS."}
+   ])
